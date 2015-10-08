94b4f5f523438542831663efc5889ec36b48d7c8
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-07 20:22:19 -0400

    Add joshing

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
 var Server = require('karma').Server;
+
+//********** build stuff *****************
 
 gulp.task('buildVendor', function(){
     gulp.src([
@@ -13,3 +16,33 @@ gulp.task('buildVendor', function(){
         .pipe(uglify())
         .pipe(gulp.dest('bist'));
 });
+
+gulp.task('buildCSS', function(){
+    gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'src/css/**/*.css'
+    ])
+        .pipe(concat('styles.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('bist'));
+});
+
+gulp.task('build', ['buildVendor', 'buildCSS']);
+
+//************************************************
+
+gulp.task('karma', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+gulp.task('jshint', function () {
+    gulp.src(['src/js/**/*.js', 'src/tests/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', ['jshint', 'karma']);
+
